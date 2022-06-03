9a418723f6ad7fbb100cf76927a5b3d93632b7ce
soleu <dlthf555@gmail.com>
2022-06-03 16:16:29 +0900

    fix : 타이틀 수정

--- src/bbangkeMonster/stage/TitleStage.java
+++ src/bbangkeMonster/stage/TitleStage.java
@@ -4,28 +4,30 @@ import bbangkeMonster.GameManager;
 
 public class TitleStage implements Stage {
 
-	GameManager gm = GameManager.getInstance();
-	@Override
-	public void init() {
+    GameManager gm = GameManager.getInstance();
 
-	}
+    @Override
+    public void init() {
 
-	@Override
-	public boolean update() {
-		System.out.println("______ ______                             _           _    ___  ___                    _               \n" +
-				"| ___ \\| ___ \\                           | |         | |   |  \\/  |                   | |              \n" +
-				"| |_/ /| |_/ /  __ _  _ __    __ _   ___ | | __  ___ | |_  | .  . |  ___   _ __   ___ | |_   ___  _ __ \n" +
-				"| ___ \\| ___ \\ / _` || '_ \\  / _` | / __|| |/ / / _ \\| __| | |\\/| | / _ \\ | '_ \\ / __|| __| / _ \\| '__|\n" +
-				"| |_/ /| |_/ /| (_| || | | || (_| || (__ |   < |  __/| |_  | |  | || (_) || | | |\\__ \\| |_ |  __/| |   \n" +
-				"\\____/ \\____/  \\__,_||_| |_| \\__, | \\___||_|\\_\\ \\___| \\__| \\_|  |_/ \\___/ |_| |_||___/ \\__| \\___||_|   \n" +
-				"                              __/ |                                                                    \n" +
-				"                             |___/                                                                   ");
-		System.out.println("[PRESS ANY BUTTON]");
-		String txt = gm.scan.next();
-		if (txt != null) {
-			gm.nextStageName = "LOBBY";
-			return true;
-		}
-		return false;
-	}
+    }
+
+    @Override
+    public boolean update() {
+        System.out.println("\n" +
+                "______ ______                       _           ___  ___                    _               \n" +
+                "| ___ \\| ___ \\                     | |          |  \\/  |                   | |              \n" +
+                "| |_/ /| |_/ /  __ _  _ __    __ _ | | __  ___  | .  . |  ___   _ __   ___ | |_   ___  _ __ \n" +
+                "| ___ \\| ___ \\ / _` || '_ \\  / _` || |/ / / _ \\ | |\\/| | / _ \\ | '_ \\ / __|| __| / _ \\| '__|\n" +
+                "| |_/ /| |_/ /| (_| || | | || (_| ||   < |  __/ | |  | || (_) || | | |\\__ \\| |_ |  __/| |   \n" +
+                "\\____/ \\____/  \\__,_||_| |_| \\__, ||_|\\_\\ \\___| \\_|  |_/ \\___/ |_| |_||___/ \\__| \\___||_|   \n" +
+                "                              __/ |                                                         \n" +
+                "                             |___/                                                          \n");
+        System.out.println("[PRESS ANY BUTTON]");
+        String txt = gm.scan.next();
+        if (txt != null) {
+            gm.nextStageName = "LOBBY";
+            return true;
+        }
+        return false;
+    }
 }
